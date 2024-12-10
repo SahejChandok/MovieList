6b0ddcb93efe6c2243e3fd8bfd0b9f90d75e1622
SahejChandok <chandoksahej@gmail.com>
2024-12-09 23:48:42 -0500

    Adding review controller and finishing up

--- movies/src/main/java/com/example/movies/controller/MovieController.java
+++ movies/src/main/java/com/example/movies/controller/MovieController.java
@@ -1,13 +1,17 @@
 package com.example.movies.controller;
 
+import com.example.movies.entity.Movie;
+import com.example.movies.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -23,5 +27,10 @@ public class MovieController {
 //            System.out.println(i.getTitle());
 //        }
         return new ResponseEntity<>(movies,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String id){
+        return new ResponseEntity<Optional<Movie>>(movieService.MovieById(id), HttpStatus.OK);
     }
 }
--- movies/src/main/java/com/example/movies/repository/ReviewRepository.java
+++ movies/src/main/java/com/example/movies/repository/ReviewRepository.java
@@ -1,10 +1,11 @@
-package com.example.movies.controller;
+package com.example.movies.repository;
 
+import com.example.movies.entity.Review;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
 
 }
